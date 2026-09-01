af4eb9cce032c15d8b3ebfbc57ebec270b3a8474
Johan Galeano <104652500+EnragedMagic@users.noreply.github.com>
2026-08-31 22:33:48 -0500

    Update ScientificEvalVisitor.java

--- ScientificEvalVisitor.java
+++ ScientificEvalVisitor.java
@@ -1,13 +1,15 @@
+// HashMap y Map es para guardar variables
 import java.util.HashMap;
 import java.util.Map;
+// ArrayList y List se usan para guardar los puntos de las graficas
 import java.util.ArrayList;
 import java.util.List;
-
+// Esta clase hereda del visitor que antlr genera, double e sque los metodos devuelven numeros decimales 
 public class ScientificEvalVisitor
         extends ScientificCalcBaseVisitor<Double> {
-
+// Memoria de las variables 
     Map<String, Double> memory = new HashMap<>();
-
+// Convierte un number reconocido por ANTLR de txt a numero double
     @Override
     public Double visitNumber(
             ScientificCalcParser.NumberContext ctx) {
@@ -16,7 +18,7 @@ public class ScientificEvalVisitor
                 ctx.NUMBER().getText()
         );
     }
-
+// Esto es para ejecutar suma o resta
     @Override
     public Double visitAddSub(
             ScientificCalcParser.AddSubContext ctx) {
@@ -30,7 +32,7 @@ public class ScientificEvalVisitor
 
         return left - right;
     }
-
+// Ejecuta multiplicacion o division
     @Override
     public Double visitMulDiv(
             ScientificCalcParser.MulDivContext ctx) {
@@ -48,14 +50,14 @@ public class ScientificEvalVisitor
 
         return left / right;
     }
-
+// Evalua lo que esta dentro de parentesis
     @Override
     public Double visitParens(
             ScientificCalcParser.ParensContext ctx) {
 
         return visit(ctx.expr());
     }
-
+// Evalua una expresion y muestra su resultado
     @Override
     public Double visitPrintExpr(
             ScientificCalcParser.PrintExprContext ctx) {
@@ -66,7 +68,7 @@ public class ScientificEvalVisitor
 
         return value;
     }
-
+// Guardar variables 
     @Override
     public Double visitAssign(
             ScientificCalcParser.AssignContext ctx) {
@@ -79,7 +81,7 @@ public class ScientificEvalVisitor
 
         return value;
     }
-
+// Recupera el valor de una variable, comprueba si existe y si no, muestra error
     @Override
     public Double visitId(
             ScientificCalcParser.IdContext ctx) {
@@ -96,7 +98,7 @@ public class ScientificEvalVisitor
 
         return 0.0;
     }
-
+// Calcular potencia
     @Override
     public Double visitPower(
             ScientificCalcParser.PowerContext ctx) {
@@ -106,11 +108,11 @@ public class ScientificEvalVisitor
 
         return Math.pow(base, exponent);
     }
-
+// Maneja signos positivos y negativos 
     @Override
     public Double visitUnary(
             ScientificCalcParser.UnaryContext ctx) {
-
+// - cambia signo
         double value = visit(ctx.expr());
 
         if (ctx.op.getText().equals("-")) {
@@ -119,7 +121,7 @@ public class ScientificEvalVisitor
 
         return value;
     }
-
+// Ejecuta funciones de un argumento
     @Override
     public Double visitFunctionCall(
             ScientificCalcParser.FunctionCallContext ctx) {
@@ -129,7 +131,7 @@ public class ScientificEvalVisitor
 
         double value =
                 visit(ctx.expr());
-
+// Decide que operacion matematica ejecutar 
         switch (function) {
 
             case "sin":
@@ -177,7 +179,7 @@ public class ScientificEvalVisitor
                 );
         }
     }
-
+// Ejecuta funciones que reciben dos argumentos
     @Override
     public Double visitFunction2Call(
             ScientificCalcParser.Function2CallContext ctx) {
@@ -185,8 +187,8 @@ public class ScientificEvalVisitor
         String function =
                 ctx.function2().getText();
 
-        double value1 = visit(ctx.expr(0));
-        double value2 = visit(ctx.expr(1));
+        double value1 = visit(ctx.expr(0)); // 1 
+        double value2 = visit(ctx.expr(1)); // 2
 
         switch (function) {
 
@@ -205,7 +207,7 @@ public class ScientificEvalVisitor
                 );
         }
     }
-
+// Convierte las constantes del lenguaje, en las constantes matematicas de java
     @Override
     public Double visitConstantExpr(
             ScientificCalcParser.ConstantExprContext ctx) {
@@ -223,7 +225,7 @@ public class ScientificEvalVisitor
 
         return 0.0;
     }
-
+// Borra todas las variables guardadas
     @Override
     public Double visitClear(
             ScientificCalcParser.ClearContext ctx) {
@@ -236,7 +238,7 @@ public class ScientificEvalVisitor
 
         return 0.0;
     }
-
+// Muestra las variables guardadas
     @Override
     public Double visitShowVars(
             ScientificCalcParser.ShowVarsContext ctx) {
@@ -249,7 +251,7 @@ public class ScientificEvalVisitor
 
             return 0.0;
         }
-
+// Recorre todas las variables 
         for (Map.Entry<String, Double> entry :
                 memory.entrySet()) {
 
@@ -262,45 +264,46 @@ public class ScientificEvalVisitor
 
         return 0.0;
     }
-
+// Este es para graficar una explesion usando xmin y xmax
     @Override
     public Double visitPlotExpr(
             ScientificCalcParser.PlotExprContext ctx) {
-
+// 2do parametro del plot
         double xmin = visit(ctx.expr(1));
+// 2er parametro del plot
         double xmax = visit(ctx.expr(2));
-
+// Cantidad de puntos que se calculan
         int samples = 800;
-
+// Listas donde se guardan los puntos
         List<Double> xs = new ArrayList<>();
         List<Double> ys = new ArrayList<>();
-
+// Guarda el valor anterior de x si existe
         Double oldX = memory.get("x");
 
         for (int i = 0; i < samples; i++) {
-
+// Calcula 800 valores diferentes de x
             double x =
                     xmin
                     + i * (xmax - xmin)
                     / (samples - 1);
-
+// Guarda temporalmente x
             memory.put("x", x);
-
+// Evalua la funcion usando ese valor de x 
             double y = visit(ctx.expr(0));
-
+// Solo guarda resultados validos
             if (Double.isFinite(y)) {
                 xs.add(x);
                 ys.add(y);
             }
         }
-
+// Devuelve x al valor que tenia ntes 
         restoreX(oldX);
-
+//Envia los puntos a plot
         new PlotWindow(xs, ys);
 
         return 0.0;
     }
-
+// Esto es igual que el plot normal, pero permite especificar ymin y ymax
     @Override
     public Double visitPlotRangeExpr(
             ScientificCalcParser.PlotRangeExprContext ctx) {
@@ -336,7 +339,7 @@ public class ScientificEvalVisitor
         }
 
         restoreX(oldX);
-
+// Los envia a plotwindow
         new PlotWindow(
                 xs,
                 ys,
@@ -346,18 +349,18 @@ public class ScientificEvalVisitor
 
         return 0.0;
     }
-
+// Grafica dos funciones al mismo tiempo
     @Override
     public Double visitPlotMultiExpr(
             ScientificCalcParser.PlotMultiExprContext ctx) {
-
+// En este plot, aqui estan ambas funciones
         double xmin = visit(ctx.expr(2));
         double xmax = visit(ctx.expr(3));
 
         int samples = 800;
 
         List<Double> xs = new ArrayList<>();
-
+// Valores de Y de cada funcion
         List<Double> ys1 = new ArrayList<>();
         List<Double> ys2 = new ArrayList<>();
 
@@ -371,7 +374,7 @@ public class ScientificEvalVisitor
                     / (samples - 1);
 
             memory.put("x", x);
-
+// Evalua ambas fuciones con el mismo x
             double y1 = visit(ctx.expr(0));
             double y2 = visit(ctx.expr(1));
 
@@ -382,7 +385,7 @@ public class ScientificEvalVisitor
         }
 
         restoreX(oldX);
-
+// Envia dos funciones a plotwindow
         new PlotWindow(
                 xs,
                 ys1,
@@ -391,9 +394,9 @@ public class ScientificEvalVisitor
 
         return 0.0;
     }
-
+// Restaura la variable x despues de graficar
     private void restoreX(Double oldX) {
-
+// Si x no existia antes del plot, la elimina y si ya existia devuelve su valor anterior
         if (oldX == null) {
             memory.remove("x");
         } else {
